12603876179eab7e041e1180649bd22b3817df34
Basemjabr23 <basemgabr65@gmail.com>
2026-08-17 19:58:30 +0300

    Delete task5.py

--- task5.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-job = input("Enter your job: ")
-city = input("Enter your city: ")
-hobby = input("Enter your hobby: ")
-
-print("\n--- Profile Card ---")
-print("Name:", name)
-print("Age:", age)
